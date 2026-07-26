ea2b1255e3108e20b551e3e3cf88eefa26e63186
Sanyul-islam <imsanyulislam@gmail.com>
2026-07-27 03:03:09 +0600

    All books Api create and added

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const PORT = 8000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+const uri = process.env.MONGODB_URI; // Your MongoDB connection string
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("book-lover");
+    const usersCollection = db.collection("user");
+    const booksCollection = db.collection("books");
+
+    // Home Route
+    app.get("/", (req, res) => {
+      res.send("Hello, your Express server is working perfectly!");
+    });
+
+    // Get all books
+    app.get("/books", async (req, res) => {
+      try {
+        const books = await booksCollection.find().toArray();
+
+        res.send(books);
+      } catch (error) {
+        res.status(500).send({ message: "Failed to fetch books." });
+      }
+    });
+   
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run().catch(console.dir);
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
